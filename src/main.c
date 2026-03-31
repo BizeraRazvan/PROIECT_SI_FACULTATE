b27e5ff462f1da7d3564173ac89d65999b40e7e3
Antonio Boboc <antonioboboc987@gmail.com>
2026-03-31 15:12:33 +0300

    ini
    Initialize timer and GPIO, toggle LED with delays.

---
+++ src/main.c
@@ -0,0 +1,31 @@
+#include "drivers/gpio/gpio.h"
+#include "drivers/timer/timer0.h"
+#include "bsp/nano.h"
+
+int main(void) {
+    
+    Timer0_Init();
+
+    
+    GPIO_Init(LED_BUILTIN, GPIO_OUTPUT);
+
+    uint32_t last_time = 0;
+
+    while (1) {
+            
+        for(int i = 0 ; i < 12 ; i++) {
+        GPIO_Toggle(LED_BUILTIN);
+        Delay(200); 
+        }
+         for(int i = 0 ; i < 12 ; i++) {
+        GPIO_Toggle(LED_BUILTIN);
+        Delay(500); 
+        }
+        for(int i = 0 ; i < 12 ; i++) {
+        GPIO_Toggle(LED_BUILTIN);
+        Delay(900); 
+        }
+        
+    }
+
+}
